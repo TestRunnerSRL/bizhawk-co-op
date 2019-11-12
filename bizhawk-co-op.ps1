b1a26629cbd6b8e3f866ccd4400c45b8c6bba5c0
TestRunner <testrunner92@gmail.com>
2019-11-12 12:33:35 -0700

    changed dev branch ps script to grab the dev branch code

--- bizhawk-co-op.ps1
+++ bizhawk-co-op.ps1
@@ -38,7 +38,7 @@ $destination.Copyhere($zip_file.items())
 Remove-Item $filename
 
 #download bizhawk-co-op
-$url = "https://github.com/TestRunnerSRL/bizhawk-co-op/archive/master.zip"
+$url = "https://github.com/TestRunnerSRL/bizhawk-co-op/archive/dev.zip"
 $filename = "bizhawk-co-op.zip"
 Invoke-WebRequest -Uri $url -OutFile $filename
 #unzip
